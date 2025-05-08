8341e58a12fc1c6039c5b9de4ce7cda74689c50d
Nedyalko <nedcoder@gmail.com>
2025-05-08 18:21:42 +0300

    new changes

--- jottingdown/settings.py
+++ jottingdown/settings.py
@@ -158,7 +158,6 @@ INVALIDATE_PREVIOUS_SESSIONS = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles"),]
 
 
 # Media files
